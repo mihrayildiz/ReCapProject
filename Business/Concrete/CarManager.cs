5071599e93ca19f0f15b2677ef77f43a24f45d4f
mihrayildiz <mihrayildiz@hotmail.com>
2024-04-30 18:37:13 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class CarManager : ICarService
+{
+   private readonly  ICarDal _cardal;
+
+    public CarManager(ICarDal cardal)
+    {
+        _cardal = cardal;
+    }
+
+    public List<Car> GetAll()
+    {
+       return  _cardal.GetAll();
+    }
+
+    public Car GetById(int id)
+    {
+        Car car = _cardal.GetById(id);
+        return car;
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+Console.WriteLine("Hello, World!");
+
+
+CarManager carManager = new CarManager(new InMemoryCarDal());
+
+foreach (var car in carManager.GetAll())
+{
+    Console.WriteLine(car.Description);
+}
+
+
+
+Console.WriteLine("------------");
+
+
+
+Car carById = carManager.GetById(2);
+
+Console.WriteLine(carById.Description);
+
