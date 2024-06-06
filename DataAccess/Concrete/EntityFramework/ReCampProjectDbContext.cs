291a54e4d20caf7cfd57f07764b4a353741ec6b6
mihrayildiz <mihrayildiz@hotmail.com>
2024-06-06 21:54:37 +0300

    Added New Entity

--- DataAccess/Concrete/EntityFramework/ReCampProjectDbContext.cs
+++ DataAccess/Concrete/EntityFramework/ReCampProjectDbContext.cs
@@ -20,6 +20,9 @@ public class ReCampProjectDbContext :DbContext
     public DbSet<Car> Cars { get; set; }
     public DbSet<Brand> Brands { get; set; }
     public DbSet<Color> Colors { get; set; }
+    public DbSet<Rental> Rentals { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Customer> Customers { get; set; }
 }
 
 
---
+++ Entities/Concrete/Rental.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete;
+
+public class Rental : IEntity
+{
+    public int Id { get; set; }
+    public int CarId { get; set; }
+    public string CustomerId { get; set; }
+
+    public DateTime RentDate { get; set; }  // (Kiralama Tarihi)
+    public DateTime? ReturnDate { get; set; }  // (Teslim Tarihi)
+
+}
+
+
+//Araba teslim edilmemişse ReturnDate null'dır.
