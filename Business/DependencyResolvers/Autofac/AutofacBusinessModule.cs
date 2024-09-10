a3a863569c35df9f47fa74d9d685d5aef4511446
mihrayildiz <mihrayildiz@hotmail.com>
2024-09-10 20:57:07 +0300

    Add Autofac

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac;
+
+public class AutofacBusinessModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+       //Brand
+        builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
+        builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
+        
+        //Car
+        builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+        builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+    }
+}
--- WepAPI/Program.cs
+++ WepAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -9,15 +12,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton<ICarService, CarManager>();
-builder.Services.AddSingleton<ICarDal, EfCarDal>();
+//builder.Services.AddSingleton<ICarService, CarManager>();
+//builder.Services.AddSingleton<ICarDal, EfCarDal>();
 
-builder.Services.AddSingleton<IColorService, ColorManager>();
-builder.Services.AddSingleton<IColorDal, EfColorDal>();
+//builder.Services.AddSingleton<IColorService, ColorManager>();
+//builder.Services.AddSingleton<IColorDal, EfColorDal>();
 
 
-builder.Services.AddSingleton<ICustomerService, CustomerManager>();
-builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
+//builder.Services.AddSingleton<ICustomerService, CustomerManager>();
+//builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(options =>
+{
+    options.RegisterModule(new AutofacBusinessModule());
+});
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
