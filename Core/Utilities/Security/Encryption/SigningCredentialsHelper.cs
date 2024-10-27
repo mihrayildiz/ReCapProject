0cda4a2e72256a4d5d7881f340d6f4a92bf8f034
mihrayildiz <mihrayildiz@hotmail.com>
2024-10-28 01:08:25 +0300

    symmetric algo change

--- Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -11,6 +11,6 @@ public  class SigningCredentialsHelper
 {
     public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
     {
-        return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+        return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
     }
 }
